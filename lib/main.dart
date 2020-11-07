49abf04327e5801a154626f720408e2a1b5c59a2
Andrei Patriota <andreipatriota@Andreis-MacBook-Air.local>
2020-11-07 17:20:38 -0300

    Stepper Widget.
    Made my first Stepper app.

--- lib/main.dart
+++ lib/main.dart
@@ -4,68 +4,52 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget{
+  App({Key key}) : super(key: key);
 
-  final _listOfWidgets = [
-    SizedBox(
-      width: 350,
-      height: 500,
-      child: Card(
-        color: Colors.blueGrey[500],
-        child: Align(
-          alignment: Alignment.center,
-          child: Text('Announcements',
-            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 26),),
-        ),
-      ),
+  @override
+  AppState createState(){
+    return AppState();
+  }
+}
+
+class AppState extends State{
+
+  var _currentStep = 0;
+  final _listOfSteps = [
+    Step(
+      title: Text('Get Ready'),
+      isActive: true,
+      content: Text('Let us begin..')
     ),
-    SizedBox(
-      width: 350,
-      height: 500,
-      child: Align(
-        alignment: Alignment.center,
-        child: Text('Birthdays',
-          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 26),),
-      ),
+    Step(
+        title: Text('Get Set'),
+        isActive: false,
+        content: Text('Ok, just a little more...')
     ),
-    SizedBox(
-      width: 350,
-      height: 500,
-      child: Card(
-        color: Colors.blueGrey[500],
-        child: Align(
-          alignment: Alignment.center,
-          child: Text('Data',
-            style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 26),),
-        ),
-      ),
+    Step(
+        title: Text('Go!'),
+        isActive: true,
+        content: Text("And, we're done")
     )
-  ];
-  final _listOfTabs = [
-    Tab(icon: Icon(Icons.announcement),),
-    Tab(icon: Icon(Icons.cake),),
-    Tab(icon: Icon(Icons.cloud),)
   ];
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'My Newest App.',
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          backgroundColor: Colors.blueGrey[900],
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey,
-            title: Text('This is my newest Flutter App.'),
-            bottom: TabBar(
-              tabs: _listOfTabs,
-            ),
-          ),
-          body: TabBarView(
-            children: _listOfWidgets,
-          ),
+      title: 'Stepper Demo.',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('This is a Stepper Widget Demo!'),
         ),
+        body : Stepper(
+          type: StepperType.vertical,
+          currentStep: _currentStep,
+          onStepContinue: _currentStep < 2  ? () => setState((){_currentStep+=1;}): null,
+          onStepCancel: _currentStep > 0 ?() => setState((){_currentStep-=1;}):null,
+          steps: _listOfSteps,
+        )
       ),
     );
   }
