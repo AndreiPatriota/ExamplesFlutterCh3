cd02ad2539afc1202e5bf4ea605d35517e1f1a79
Andrei Patriota <andreipatriota@Andreis-MacBook-Air.local>
2020-11-12 22:53:00 -0300

    ThemeData at the MaterialApp Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -5,73 +5,45 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   title: 'My app',
   home: MyApp(),
+  theme: ThemeData(
+    //primarySwatch: Colors.blueGrey,
+    primaryColor: Colors.blueGrey,
+    accentColor: Colors.black87,
+    canvasColor: Colors.blueGrey[900],
+    brightness: Brightness.dark,
+    primaryTextTheme: TextTheme(
+      title: TextStyle(
+        color: Colors.deepOrange
+      )
+    )
+  ),
 )
 );
 
 class MyApp extends StatelessWidget{
 
   @override
-  Widget build(BuildContext context) {
-
-    final _listOfWidgets = [
-      Container(height: 90,),
-      Theme(
-          data: ThemeData(
-            accentColor: Colors.blue,
-          ),
-          child: Container(
-            color: Theme.of(context).accentColor,
-            child: Opacity(
-              opacity: 0.25,
-              child: Text(
-                'Text with a background color',
-                style: Theme.of(context).textTheme.title,
-              ),
-            )
-          )
-      ),
-      Container(height: 80,),
-      DecoratedBox(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF000000), Color(0xFFFF0000)],
-            tileMode: TileMode.repeated
-          )
-        ),
-        child: Container(
-          width: 200,
-          height: 200,
-          child: Text(
-            'Hello there. Zuko here',
-            style: TextStyle(color: Colors.white),
-          ),
+  Widget build(BuildContext context) =>
+    Scaffold(
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.center,
+          child: Text('Flutter App'),
         ),
       ),
-      Container(height: 80,),
-      Center(
-        child: Container(
-          color: Colors.indigo,
-          child: Transform(
-            alignment: Alignment.bottomLeft,
-            transform: Matrix4.skewY(0.4)..rotateZ(-3/12.0),
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              color: Colors.red,
-              child: Text('I am from Itapetim!!'),
-            ),
-          ),
-        ),
-      )
-    ];
-
-    return Scaffold(
       body: Center(
-        child: Column(
-          children: _listOfWidgets,
+        child: Text(
+          'Hello World from the Moon.',
+          style: Theme.of(context).primaryTextTheme.title,
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          print('Hello little fellow!!');
+        },
+        tooltip: 'Greetings',
+        child: Icon(Icons.add),
+      ),
     );
-  }
+
 }
