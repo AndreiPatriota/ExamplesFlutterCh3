2c601a22bebdfddb8bc7387887006f401bf1391a
Andrei Patriota <andreipatriota@Andreis-MacBook-Air.local>
2020-11-13 22:31:21 -0300

    Animated Container.

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,57 @@ void main() => runApp(MaterialApp(
 )
 );
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
+  MyApp({Key key}) : super(key: key);
+
+  _MyApp createState()=> _MyApp();
+}
+
+class _MyApp extends State{
+
+  var _color = Colors.amber;
+  var _height = 200.0;
+  var _width = 200.0;
+  var _hasBeenActivated = false;
+  var _btnLabel = 'Animate me';
 
   @override
   Widget build(BuildContext context) =>
-    Scaffold(
-      appBar: AppBar(
-        title: Align(
-          alignment: Alignment.center,
-          child: Text('Flutter App'),
+      Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                duration: const Duration(seconds: 2),
+                color: _color,
+                width: _width,
+                height: _height,
+              ),
+              Container(height: 50,),
+              RaisedButton(
+                onPressed: (){
+                  if(!_hasBeenActivated){
+                    _color = Colors.green;
+                    _width = 400.0;
+                    _height = 400.0;
+                    _hasBeenActivated = true;
+                    _btnLabel = 'Take me back!';
+                  }
+                  else{
+                    _color = Colors.amber;
+                    _width = 200.0;
+                    _height = 200.0;
+                    _hasBeenActivated = false;
+                    _btnLabel = 'Animate me!';
+                  }
+                  setState(() {});
+                },
+                child: Text(_btnLabel),
+              )
+            ],
+          ),
         ),
-      ),
-      body: Center(
-        child: Text(
-          'Hello World from the Moon.',
-          style: Theme.of(context).primaryTextTheme.title,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          print('Hello little fellow!!');
-        },
-        tooltip: 'Greetings',
-        child: Icon(Icons.add),
-      ),
-    );
+      );
 
 }
